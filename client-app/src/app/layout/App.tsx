a6380552a2135601f2c27e8cc8d7d392dd1ba95b
Derrick Freeman <dfreeman@costar.com>
2023-06-01 16:53:20 -0400

    Axios added

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
   const [selectActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const [Loading , setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(
-      response => {
-        setActivities(response.data)
+    agent.Activities.list().then(response => {
+      let activities: Activity[] = response.map(activity => { 
+        activity.startDateTime =  activity.startDateTime.split('T')[0];
+        activity.endDateTime =  activity.endDateTime.split('T')[0];
+        return {...activity};
+      })
+        setActivities(activities)
+        setLoading(false);
       }
     )
   }, [])
@@ -37,17 +45,36 @@ function App() {
   }
 
   function handleCreateOrEditActivity(activity: Activity) {
-    activity.id ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
-    : setActivities([...activities, {...activity, id: uuid()}])
-    setEditMode(false);
-    setSelectedActivity(activity);
+    setSubmitting(true);
+    if (activity.id) {
+      agent.Activities.update(activity).then(() => {
+        setActivities([...activities.filter(x => x.id !== activity.id), activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    } else {
+      activity.id= uuid();
+      agent.Activities.create(activity).then(() => {
+        setActivities([...activities, activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    }
   }
 
   function handleDeleteActivity(id: string) {
-    setActivities([...activities.filter(x => x.id !== id)])
-    setEditMode(false);
-    setSelectedActivity(undefined);
+    setSubmitting(true);
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter(x => x.id !== id)])
+      setSubmitting(false);
+      setEditMode(false);
+      setSelectedActivity(undefined);
+    })
   }
+
+  if (Loading) return <LoadingComponent content='Loading activites...' />
 
   return (
     <>
@@ -64,6 +91,7 @@ function App() {
           closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
+          submitting={submitting}
         />
       </Container>
 
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,9 +6,10 @@ interface Props{
     activity: Activity | undefined;
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
+    submitting: boolean;
 }
 
-export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit}: Props) {
+export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit, submitting}: Props) {
 
     const initialState = selectedActivity || {
         id: '',
@@ -37,10 +38,10 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
                 <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
                 <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange} />
                 <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange} />
-                <Form.Input placeholder='Start Date/Time' value={activity.startDateTime} name='startDateTime' onChange={handleInputChange} />
-                <Form.Input placeholder='End Date/Time' value={activity.endDateTime} name='endDateTime' onChange={handleInputChange} />
+                <Form.Input type='date' placeholder='Start Date/Time' value={activity.startDateTime} name='startDateTime' onChange={handleInputChange} />
+                <Form.Input type='date' placeholder='End Date/Time' value={activity.endDateTime} name='endDateTime' onChange={handleInputChange} />
                 <Form.Input placeholder='Address' value={activity.address} name='address' onChange={handleInputChange} />
-                <Button floated='right' positive type='submit' content='Submit' />
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' positive content='Cancel' />
             </Form>
         </Segment>
